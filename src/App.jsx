a41e8a4bf6930ec862cca3f4328a34def05788e7
Ishitaa Dhingra <ishitaadhingra@gmail.com>
2024-03-05 02:03:54 +0530

    fetching data with react router loaders

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Menu from "./features/menu/Menu";
+import Menu, { loader as MenuLoader } from "./features/menu/Menu";
 import Home from "./ui/Home";
 import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
@@ -17,6 +17,7 @@ const router = createBrowserRouter([
       {
         path: "/menu",
         element: <Menu />,
+        loader: MenuLoader,
       },
       {
         path: "/cart",
